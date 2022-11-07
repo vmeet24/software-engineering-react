b41cd1e4e368264b920a884821d3566ee187195e
vmeet24 <meetvora1998@gmail.com>
2022-11-06 20:55:11 -0500

    tests updated

--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -1,5 +1,6 @@
 import { createTuit, findAllTuits, deleteTuit, findTuitById } from "../services/tuits-service";
-import { createUser, deleteUsersByUsername } from "../services/users-service"
+import { createUser, deleteUsersByUsername, deleteUser } from "../services/users-service"
+
 
 describe('can create tuit with REST API', () => {
   const tuit = {
@@ -93,40 +94,47 @@ describe('can retrieve a tuit by their primary key with REST API', () => {
 });
 
 describe('can retrieve all tuits with REST API', () => {
-  const tuits = [
-    { tuit: "Alice's tuit 0" },
-    { tuit: "Alice's tuit 1" },
-    { tuit: "Alice's tuit 2" }
-  ]
 
   const ripley = {
-    username: 'ellenripley',
+    username: 'ellenripley123456',
     password: 'lv426',
     email: 'ellenripley@aliens.com'
   };
 
+  const mapTuitString = ['Tuit1', 'Tuit2', 'Tuit3'];
+  const mapTuitId = [];
+
   beforeAll(async () => {
-    await deleteUsersByUsername(ripley.username);
     const user = await createUser(ripley);
-    ripley["id"] = user._id;
-    tuits.forEach(async (tuit) => {
-      const newTuit = await createTuit(ripley["id"], tuit);
-      tuit["id"] = newTuit._id;
+    ripley.uniqueId = user._id;
+
+    mapTuitString.map(async tuit => {
+      await createTuit(ripley.uniqueId,
+        {
+          tuit: `${tuit}`
+        }
+      );
     })
   });
 
-  test('can delete tuit with REST API', async () => {
-    const getTuits = await findAllTuits();
-    expect(getTuits.length).toBe(tuits.length);
-    getTuits.forEach(tuit => {
-      expect(tuits.includes(x => x.id === tuit._id)).toBeTruthy();
-    })
-  })
-
   afterAll(async () => {
-    tuits.forEach(async (tuit) => {
-      await deleteTuit(tuit["id"]);
-    })
-    await deleteUsersByUsername(ripley.username);
-  })
+    await mapTuitId.forEach(tuit => {
+      deleteTuit(tuit);
+    });
+    await deleteUser(ripley.uniqueId);
+  });
+
+  test('can retrieve all tuits with REST API', async () => {
+
+    const findTuit = await findAllTuits();
+
+    const tuitsWeInserted = findTuit.filter(
+      tuit => mapTuitString.indexOf(tuit.tuit) >= 0);
+
+    tuitsWeInserted.forEach(tuit => {
+      const tuitName = mapTuitString.find(tuitName => tuitName === tuit.tuit);
+      expect(tuit.tuit).toEqual(tuitName);
+      mapTuitId.push(tuit._id);
+    });
+  });
 });
