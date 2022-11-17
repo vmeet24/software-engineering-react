39fc465b0e893ba0ec59e7d7bf8e810f072694c6
vmeet24 <meetvora1998@gmail.com>
2022-11-17 14:33:36 -0500

    added userDislikesTuit in like service

--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -10,3 +10,13 @@ const api = axios.create({
 export const userTogglesTuitLikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/likes/${tid}`)
         .then(response => response.data);
+
+/**
+ * Update that the user disliked the tuit
+ * @param uid user id
+ * @param tid tuit id
+ * @returns dislike object
+ */
+export const userDislikesTuit = (uid, tid) =>
+    api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
+        .then(response => response.data);
