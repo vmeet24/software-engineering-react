e37a0255f9106397e9b984798f5ebbbaf34a6603
vmeet24 <meetvora1998@gmail.com>
2022-11-17 00:57:02 -0500

    added support for like in tuit component class

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
+import * as likesService from "../../services/likes-service";
 
-function Tuits({tuits = [], deleteTuit}) {
-    return (
+function Tuits({ tuits = [], deleteTuit, refreshTuits }) {
+  const likeTuit = (tuit) =>
+    likesService
+      .userTogglesTuitLikes("me", tuit._id)
+      .then(refreshTuits)
+      .catch(e => alert(e))
+
+  return (
     <div>
-      <ul className="ttr-tuits list-group">
+      <ul>
         {
-          tuits.map && tuits.map(tuit => {
-            return (
-              <Tuit key={tuit._id} deleteTuit={deleteTuit} tuit={tuit}/>
-            );
-          })
+          tuits.map(tuit =>
+            <Tuit key={tuit._id}
+              deleteTuit={deleteTuit}
+              likeTuit={likeTuit}
+              tuit={tuit} />)
         }
       </ul>
     </div>
