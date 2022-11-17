1d6e12a547b31ce2279e70b02445d4cb999cb5f4
vmeet24 <meetvora1998@gmail.com>
2022-11-16 19:22:45 -0500

    added login/signup functionality

--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -15,6 +15,7 @@ import More from "../more";
 import {Login} from "../profile/login";
 import Movies from "../movies";
 import MovieDetails from "../movies/details";
+import Signup from "../profile/signup";
 
 function Tuiter () {
   return(
@@ -28,6 +29,7 @@ function Tuiter () {
             <Routes>
               <Route path="/" element={<Home/>}/>
               <Route path="/login" element={<Login/>}/>
+              <Route path="/signup" element={<Signup/>}/>
               <Route path="/tuiter" element={<Home/>}/>
               <Route path="/tuiter/:uid" element={<Home/>}/>
               <Route path="/home" element={<Home/>}/>
